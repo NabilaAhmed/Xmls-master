dda976b6786f0399a29db6748bbfc26e4e884706
NabilaAhmed <nabilaahmed101401@yahoo.com>
2016-11-25 19:01:11 +0200

    Initial commit

---
+++ app/src/main/java/com/example/s/myapplication/list_course.java
@@ -0,0 +1,20 @@
+package com.example.s.myapplication;
+
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toolbar;
+
+public class list_course extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_course);
+        final Toolbar toolbar=(Toolbar) findViewById(R.id.MyToolbar);
+        //setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        CollapsingToolbarLayout collapsingToolbarLayout=(CollapsingToolbarLayout) findViewById(R.id.collapse_toolbar);
+        collapsingToolbarLayout.setTitle("MyToolbar");
+    }
+}
